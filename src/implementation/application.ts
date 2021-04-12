38fc169dfc0a1f5c2f95d8fd824606ac090a5b30
André <abs@emancipe.se>
2021-04-12 03:04:15 -0300

    better style for implementation type declaration

--- src/implementation/application.ts
+++ src/implementation/application.ts
@@ -1,10 +1,12 @@
 import { IApplication, ILogger } from '../language'
 
-export const NodeApplication = (logger: Readonly<ILogger>) => {
+export const NodeApplication = (
+  logger: Readonly<ILogger>
+): Readonly<IApplication> => {
   return {
     main: (argv: ReadonlyArray<string>) => {
       logger.log(argv.join(' '))
       return 0
     }
-  } as Readonly<IApplication>
+  }
 }
--- src/implementation/consolelogger.ts
+++ src/implementation/consolelogger.ts
@@ -1,11 +1,11 @@
 import { ILogger } from '../language'
 
-export const ConsoleLogger = () => {
+export const ConsoleLogger = (): Readonly<ILogger> => {
   let count = 0
   return {
     log: (msg: Readonly<string>) => {
       count = count + 1
       console.log(`${count}: ${msg})`)
     }
-  } as Readonly<ILogger>
+  }
 }
