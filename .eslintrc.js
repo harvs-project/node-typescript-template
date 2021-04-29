1e7939b99dd43c70a44d6d398d943d852cfe3a99
André <abs@emancipe.se>
2021-04-28 23:10:52 -0300

    use a more general environment for modules in the root dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  env: {
-    node: true,
-    es2020: true
-  }
+  env: { commonjs: true },
+  extends: ['eslint:recommended']
 }
